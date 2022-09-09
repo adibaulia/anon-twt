bf2adbac933d2cc29dccabcd50d148eb97fc84a3
adibaulia <adib.aulia@gli.id>
2022-09-09 10:59:31 +0700

    done for stating and chatting 2 person

--- internal/services/services.go
+++ internal/services/services.go
@@ -1,13 +1,11 @@
 package services
 
 import (
-	"context"
 	"fmt"
 
 	"github.com/adibaulia/anon-twt/config"
 	"github.com/adibaulia/anon-twt/internal/models"
 	"github.com/dghubble/go-twitter/twitter"
-	twitterV2 "github.com/g8rswimmer/go-twitter/v2"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -33,75 +31,77 @@ func NewService(client *config.Connection) *svc {
 func (s *svc) DirectMessagesEventProcessor(event twitter.DirectMessageEvent) error {
 	senderID := event.Message.SenderID
 
-	opts := twitterV2.UserLookupOpts{
-		Expansions: []twitterV2.Expansion{twitterV2.ExpansionPinnedTweetID},
-	}
-
-	userResponse, err := s.client.TwtCliV2.UserLookup(context.Background(), []string{senderID}, opts)
-	if err != nil {
-		log.Panicf("user lookup error: %v", err)
-	}
-
-	dictionaries := userResponse.Raw.UserDictionaries()
-
-	userData := dictionaries[senderID]
-
 	if senderID == SelfTwitterID {
 		return nil
 	}
 	switch event.Message.Data.Text {
 	case "/start":
-		func() {
+		s.sendDirectMessage(senderID, &twitter.DirectMessageData{
+			Text: "Searching...",
+		})
+		for {
 			convos.Lock()
-			defer convos.Unlock()
-			for {
-				users := convos.Users
-				if _, found := users[senderID]; !found {
-					func() {
-						users[senderID] = models.UserConvo{
-							TwittID:  senderID,
-							Name:     userData.User.Name,
-							Username: userData.User.UserName,
-							Status:   models.Ready,
-						}
-						convos.Users = users
-					}()
-					continue
-				} else {
-					found := false
-					for id, targetUser := range users {
-						if targetUser.Status == models.Ready && id != senderID {
-							func() {
-								user := users[senderID]
-								user.TargetTwittID = targetUser.TwittID
-								user.Status = models.InConvo
-								users[senderID] = user
-								convos.Users = users
-							}()
-							found = true
-						}
-					}
-					if !found {
-						continue
-					} else {
-						break
-					}
+			users := convos.Users
+			curUser := users[senderID]
+			curUser.Status = models.Ready
+			targetTwittID := ""
+			for targetID, user := range users {
+				if user.TwittID != senderID && (user.Status == models.Ready || (user.Status == models.InConvo && user.TargetTwittID == senderID)) && curUser.Status == models.Ready {
+					curUser.Status = models.InConvo
+					curUser.TargetTwittID = user.TwittID
+					targetTwittID = targetID
 				}
 			}
-		}()
+			users[senderID] = curUser
+			convos.Users = users
+
+			if curUser.TargetTwittID == targetTwittID && users[targetTwittID].TargetTwittID == senderID {
+				convos.Unlock()
+				break
+			}
+			convos.Unlock()
+		}
 		s.sendDirectMessage(senderID, &twitter.DirectMessageData{
-			Text: "ready to go",
+			Text: "Settled and ready to chat!",
 		})
+
+	default:
+		text := event.Message.Data.Text
+		convos.Lock()
+		users := convos.Users
+		curUser := users[senderID]
+		if curUser.Status == models.InConvo {
+			s.sendDirectMessage(curUser.TargetTwittID, &twitter.DirectMessageData{
+				Text: text,
+			})
+		}
+		convos.Unlock()
 	}
+
 	return nil
 }
 
 func (s *svc) SendWelcomeMessage(event models.FollowEvent) error {
 	if event.Type == Follow {
-		follower := event.Target
+		user := event.Target
 		log.Infof("Welcome Message triggered")
-		s.sendDirectMessage(follower.ID, &twitter.DirectMessageData{
-			Text:       wellcomeMessage(follower.Name),
+
+		users := convos.Users
+		if _, found := users[user.ID]; !found {
+			func() {
+				convos.Lock()
+				defer convos.Unlock()
+				users[user.ID] = models.UserConvo{
+					TwittID:  user.ID,
+					Name:     user.Name,
+					Username: user.ScreenName,
+					Status:   models.End,
+				}
+				convos.Users = users
+			}()
+		}
+		s.sendDirectMessage(user.ID, &twitter.DirectMessageData{
+			Text:       wellcomeMessage(user.Name),
 			QuickReply: defaultQuickReply(),
 		})
 	}
